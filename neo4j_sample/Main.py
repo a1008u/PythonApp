fdc28e47880558f44282045024c5f48f99e15b48
akira uemoto <empty.aauu0001@gmail.com>
2019-02-04 00:09:54 +0900

    結果確認用の追加

--- neo4j_sample/Main.py
+++ neo4j_sample/Main.py
@@ -3,6 +3,7 @@ from neo4j_sample import DeleteNeo4j as clear
 from neo4j_sample import Input_neo4j as input_neo4j
 from neo4j_sample import InputFile as inputFile
 
+# MATCH p=(x:log)-[*1..2]->(c:v3ex) RETURN p 結果の確認用
 if __name__ == '__main__':
     driver: Driver = GraphDatabase.driver('bolt://localhost:7687')
     with driver.session() as session:
